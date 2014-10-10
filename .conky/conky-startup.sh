3751b0280a1110b2b6acc5e0d65fb6609b460417
vkyuu <lanvrc0@gmail.com>
2014-10-10 20:04:43 +0700

    First Commit

---
+++ .conky/conky-startup.sh
@@ -0,0 +1,12 @@
+sleep 5s
+killall conky
+cd "/home/kyuu/.conky/1/config"
+conky -c "/home/kyuu/.conky/1/config/gambar" &
+cd "/home/kyuu/.conky/1/config"
+conky -c "/home/kyuu/.conky/1/config/jam" &
+cd "/home/kyuu/.conky/1/config"
+conky -c "/home/kyuu/.conky/1/config/jam2" &
+cd "/home/kyuu/.conky/1/config"
+conky -c "/home/kyuu/.conky/1/config/jamkecil" &
+cd "/home/kyuu/.conky/1/config"
+conky -c "/home/kyuu/.conky/1/config/ram" &
